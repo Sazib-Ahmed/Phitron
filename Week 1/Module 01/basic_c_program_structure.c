663dcd3b53abfc248ca2c8bc7f8c1a405e90ca2b
Sazib-Ahmed <mail.sazibahmed@gmail.com>
2024-09-29 09:36:28 +0600

    Update input and output files with new values

---
+++ Week 1/Module 01/basic_c_program_structure.c
@@ -0,0 +1,30 @@
+//documentation section
+/*author : MD. Sazib Ahmed
+code: a basic c program structue
+*/
+
+//this is a comment
+
+//linking section
+#include<stdio.h>
+#include<math.h>
+
+//defination section
+#define PI 3.1416
+#define ll long long
+
+//global declaration section
+int n=100;
+int sum(int x, int y);
+
+//main function section
+int main(){
+    //statements:
+    return 0;
+}
+
+//sub program setion
+int sum(int x, int y){
+    return x+y;
+}
+
