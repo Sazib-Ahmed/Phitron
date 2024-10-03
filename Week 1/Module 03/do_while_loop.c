fc1b08b0f66de2a297e7aa1a6c64d43daa55d279
MD. Sazib Ahmed <mail.sazibahmed@gmail.com>
2024-10-04 04:42:02 +0600

    Add C program with for loop

---
+++ Week 1/Module 03/do_while_loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int n=0, i=0;
+    scanf("%d", &n);
+
+    do{
+    printf("I am a Muslim\n");
+    i++;
+    }
+    while(i<n);
+
+    return 0;
+}
---
+++ Week 1/Module 03/while_loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int n=0, i=0;
+    scanf("%d", &n);
+
+    while(i<n)
+    {
+        printf("I am a Muslim.\n");
+        i+=1;
+    }
+
+}
