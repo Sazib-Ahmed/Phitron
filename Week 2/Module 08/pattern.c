9a20442b762f7b90ef30bec2f566af1f655dbc9c
Sazib-Ahmed <mail.sazibahmed@gmail.com>
2024-10-16 14:50:02 +0600

    Update input.txt and output.txt files, and modify pattern.c
    - Update input.txt file to change the value from 5 to 3
    - Update output.txt file to modify the numbers in the list
    - Modify pattern.c to add a nested loop for printing a pattern

--- Week 2/Module 08/pattern.c
+++ Week 2/Module 08/pattern.c
@@ -11,4 +11,14 @@ int main(){
         printf("\n");
     }
 
+    for(int i=1; i<n; i++){
+        for(int j=1; j<=i; j++){
+            printf(" ");
+        }
+        for(int j=1; j<=n-i; j++){
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
 }
