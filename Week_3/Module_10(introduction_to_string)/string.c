be428843cabc9ef4f0ae1628070a26440864b7b3
Sazib-Ahmed <mail.sazibahmed@gmail.com>
2024-10-21 19:34:21 +0600

    Refactor string formatting in output.txt and string.c

--- Week_3/Module_10(introduction_to_string)/string.c
+++ Week_3/Module_10(introduction_to_string)/string.c
@@ -6,7 +6,7 @@ int main() {
     //scanf("%[^\n]s", str);
 
     //escape sequence
-    char str1[] = "Bangladesh \n\tis a \"beautiful\" country. \0 and ...";
+    char str1[] = "'Bangladesh' \n\tis a \"beautiful\" country.\\n \0 and ...";
 
     //just gets and puts is less relaiable better alternative is fgets and fputs.
     fgets(str, sizeof(str), stdin);
