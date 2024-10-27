175cbc6e92df7dd3bcc4984c9797713ddc1e53c6
Sazib-Ahmed <mail.sazibahmed@gmail.com>
2024-10-27 15:14:02 +0600

    Refactor string formatting in input.txt, output.txt, and test.c

--- Week_4/Module_13(frequency_arary)/v-frequency_array.c
+++ Week_4/Module_13(frequency_arary)/v-frequency_array.c
@@ -1,3 +1,33 @@
+//with frequency array
+
+#include <stdio.h>
+
+int main() {
+    int n = 0, m = 0, count;
+    scanf("%d %d", &n, &m);
+
+    m++;
+    int a[n], f[m];
+    for (int i = 0; i < m; i++) {
+        f[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        f[a[i]]++;
+    }
+
+    for (int i = 1; i < m ; i++) {
+        printf("%d\n", f[i]);
+    }
+
+    return 0;
+}
+
+
+/*
+//without frequency array
+
 #include <stdio.h>
 
 int main() {
@@ -22,7 +52,7 @@ int main() {
 
     return 0;
 }
-
+*/
 
 /*
 https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/V
