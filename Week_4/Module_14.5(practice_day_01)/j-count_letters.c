765f9834d5b4b9c9afaddcc39b10b4f79616194e
MD. Sazib Ahmed <mail.sazibahmed@gmail.com>
2024-10-31 03:44:33 +0600

    Refactor string formatting in input.txt, output.txt, and test.c

---
+++ Week_4/Module_14.5(practice_day_01)/j-count_letters.c
@@ -0,0 +1,34 @@
+
+
+/*
+https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/J
+
+J. Count Letters
+time limit per test2 seconds
+memory limit per test64 megabytes
+Given a string S. Determine how many times does each letter occurred in S.
+
+Input
+Only one line contains the string S (1 ≤ |S| ≤ 107) where |S| is the length of the string and it consists of only lowercase English letters.
+
+Output
+For each character that appears in S, print a single line that contains the following format: "X : Y" where X is the letter and Y is the number of times that letter X occurred in S.
+
+Note: you must print letters in ascending order.
+
+Examples
+InputCopy
+aaabbc
+OutputCopy
+a : 3
+b : 2
+c : 1
+InputCopy
+regff
+OutputCopy
+e : 1
+f : 2
+g : 1
+r : 1
+
+*/
