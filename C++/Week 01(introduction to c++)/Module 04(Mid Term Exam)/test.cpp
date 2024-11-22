93fadcaee6e50c8af4a0d3932af971f5efbccc70
MD. Sazib Ahmed <mail.sazibahmed@gmail.com>
2024-11-22 23:02:41 +0600

    Add student information processing logic to determine highest marks

--- C++/Week 01(introduction to c++)/Module 04(Mid Term Exam)/test.cpp
+++ C++/Week 01(introduction to c++)/Module 04(Mid Term Exam)/test.cpp
@@ -1,9 +1,129 @@
 /*
-------------------------------------
 ====================================
+Who Is It
+Problem
+Submissions
+Leaderboard
+Discussions
+Problem Statement
+
+A student has several pieces of information, such as a unique ID, name, section, and total marks. You will be given the information of three students. Your task is to determine and print the details of the student who achieved the highest total marks. In the case of a tie (i.e., two or more students having the same total marks), print the information of the student with the smaller ID.
+
+Input Format
+
+First line will contain T, the number of test cases.
+For each test case there will be 3 lines. Each line will contain - ID, Name, Section, Total Marks of a student. The name will contain lowercase English alphabets only.
+Constraints
+
+1 <= T <= 1000
+1 <= ID <= 3
+1 <= |Name| <= 100
+'A' <= Section <= 'Z'
+0 <= Total Marks <= 100
+Output Format
+
+Ouptut the information as asked in the question.
+Sample Input 0
+
+3
+1 sakib A 50
+2 rakib D 96
+3 akib C 90
+1 sakib A 50
+2 rakib D 96
+3 akib C 96
+1 sakib A 50
+2 rakib D 50
+3 akib C 40
+Sample Output 0
+
+2 rakib D 96
+2 rakib D 96
+1 sakib A 50
 ------------------------------------
-====================================
-------------------------------------
+#include <bits/stdc++.h>
+
+using namespace std;
+class Student{
+    public:
+    int id;
+    char name[105];
+    char sec;
+    int marks;
+};
+
+
+int main()
+{
+    int t = 0;
+    cin >> t;
+    
+    for (int i= 0; i < t; i++) {
+        Student* s1 = new Student();
+        Student* s2 = new Student();
+        Student* s3 = new Student();
+        
+        cin >> s1->id >> s1->name >> s1->sec >> s1->marks;
+        cin >> s2->id >> s2->name >> s2->sec >> s2->marks;
+        cin >> s3->id >> s3->name >> s3->sec >> s3->marks;
+        
+        if((s1->marks > s2->marks && s1->marks > s3->marks) || 
+            (s1->marks == s2->marks && s1->id < s2->id && s1->marks > s3->marks) ||
+            (s1->marks == s3->marks && s1->id < s3->id && s1->marks > s2->marks) ||
+            (s1->marks == s2->marks && s1->marks == s3->marks && s1->id < s2->id && s1->id < s3->id)){
+            cout << s1->id << ' ' << s1->name << ' ' << s1->sec << ' ' << s1->marks;
+        }
+        else if((s2->marks > s3->marks && s2->marks > s1->marks) ||
+            (s2->marks == s3->marks && s2->id < s3->id && s2->marks > s1->marks) ||
+            (s2->marks == s1->marks && s2->id < s1->id && s2->marks > s3->marks) ||
+            (s2->marks == s3->marks && s2->marks == s1->marks && s2->id < s3->id && s2->id < s1->id)){
+            cout << s2->id << ' ' << s2->name << ' ' << s2->sec << ' ' << s2->marks;
+        }
+        else{
+            cout << s3->id << ' ' << s3->name << ' ' << s3->sec << ' ' << s3->marks;
+        }
+        cout << endl;
+    } 
+
+    return 0;
+}
+
+// //more optimized
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// class Student {
+//     public:
+//         int id;
+//         char name[105];
+//         char sec;
+//         int marks;
+// };
+
+// int main() {
+//     int t = 0;
+//     cin >> t;
+
+//     for (int i = 0; i < t; i++) {
+//         Student s[3];
+
+//         for (int i = 0; i < 3; ++i) {
+//             cin >> s[i].id >> s[i].name >> s[i].sec >> s[i].marks;
+//         }
+
+//         Student highest = s[0];
+//         for (int i = 1; i < 3; ++i) {
+//             if (s[i].marks > highest.marks || (s[i].marks == highest.marks && s[i].id < highest.id)) {
+//                 highest = s[i];
+//             }
+//         }
+        
+//         cout << highest.id << ' ' << highest.name << ' ' << highest.sec << ' ' << highest.marks << endl;
+//     }
+
+//     return 0;
+// }
 ====================================
 Choose Three
 Problem
