53c075b4b5f3980a8c663ed17d537e1c09927401
Sazib-Ahmed <mail.sazibahmed@gmail.com>
2025-02-28 02:22:24 +0600

    Refactor print_arr function to use recursion and add index parameter for improved readability

--- C++/Week 02(String, Class, Objects)/Module 07(Array of objects)/test.cpp
+++ C++/Week 02(String, Class, Objects)/Module 07(Array of objects)/test.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void print_arr(char *arr){
-    for (int i = 0; i < 4; i++) {
-        cout << *(arr+i) << ' ';
-    }
-
-
+void print_arr(char *arr, int i){
+    if (i >= 4)
+        return;
+    cout << *(arr + i) << ' ';
+    print_arr(arr,++i);
+    
 }
 
 int main() {
     char arr[] = {'a', 'b', 'c', 'd'};
     char *ptr = arr;
-    print_arr(ptr);
+
+    print_arr(ptr, 0);
+
     cout << endl;
     for (int i = 0; i < 4; i++) {
         cout << *(arr+i) << ' ';
