f5cbe86ad624d9a375b075aaf503fd62f8602d27
Sazib-Ahmed <mail.sazibahmed@gmail.com>
2025-04-08 13:12:00 +0600

    updated subset sum

--- Introduction to Algorithms/Week 05(Bonus Week)/Module 17(KnapSack Variation)/subset_sum_17_2.cpp
+++ Introduction to Algorithms/Week 05(Bonus Week)/Module 17(KnapSack Variation)/subset_sum_17_2.cpp
@@ -3,13 +3,14 @@ using namespace std;
 int a[1005];
 
 bool subset_sum(int i, int sum) {
+
     if (i < 0) {
         if (sum == 0)
             return true;
         else return false;
     }
 
-    if(i <= sum) {
+    if(a[i] <= sum) {
         bool opt1 = subset_sum(i-1, sum - a[i]);
         bool opt2 = subset_sum(i-1, sum);
         return opt1 || opt2;
